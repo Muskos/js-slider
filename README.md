4b575cb54e1b9b85836d066e3976c9c4150fa55d
muskos <a.d.makas@gmail.com>
2017-08-22 16:55:34 +0300

    Add dist
    Create Readme, move init code from plugin,
    add dist.

---
+++ README.md
@@ -0,0 +1,15 @@
+**js-slider**
+
+Native JavaScript Slider
+
+Example - https://muskos.github.io/js-slider/
+
+Browser support - chrome, ff, edge, opera, ie11
+
+````
+let caseSliderContainer = document.getElementById('jsSlider'),
+  caseSliderContainer2 = document.getElementById('jsSlider2');
+
+    new Slider(caseSliderContainer);
+    new Slider(caseSliderContainer2);
+````
--- src/js/index.js
+++ src/js/index.js
@@ -1,194 +1,190 @@
-let Slider = window.Slider || {};
+(function(){
+  let Slider;
 
-Slider = (function () {
+  Slider = (function () {
 
-  function Slider(element, settings) {
-    let _ = this;
+    function Slider(element, settings) {
+      let _ = this;
 
-    _.defaults = {
-      counts: {
-        desktop: {
-          width: 1024,
-          count: 3
+      _.defaults = {
+        counts: {
+          desktop: {
+            width: 1024,
+            count: 3
+          },
+          tablet: {
+            width: 768,
+            count: 3
+          },
+          mobile: {
+            width: 400,
+            count: 1
+          },
         },
-        tablet: {
-          width: 768,
-          count: 3
-        },
-        mobile: {
-          width: 400,
-          count: 1
-        },
-      },
-      buttons: {
-        next: {
-          classname: 'next'
-        },
-        prev: {
-          classname: 'prev'
+        buttons: {
+          next: {
+            classname: 'next'
+          },
+          prev: {
+            classname: 'prev'
+          }
+        }
+      };
+      _.slider = element;
+
+      // ie11 hack
+      for (let nextKey in settings) {
+        if (Object.prototype.hasOwnProperty.call(settings, nextKey)) {
+          _.defaults[nextKey] = settings[nextKey];
         }
       }
-    };
-    _.slider = element;
 
-    // ie11 hack
-    for (let nextKey in settings) {
-      if (Object.prototype.hasOwnProperty.call(settings, nextKey)) {
-        _.defaults[nextKey] = settings[nextKey];
-      }
+      _.options = _.defaults;
+
+      _.init();
     }
 
-    _.options = _.defaults;
+    return Slider;
+  }());
 
-    _.init();
-  }
+  Slider.prototype.init = function() {
+    let _ = this;
 
-  return Slider;
-}());
-
-Slider.prototype.init = function() {
-  let _ = this;
-
-  _.buildDom();
-  _.buildBtn();
-  _.initEvent();
-
-  window.onresize = function () {
     _.buildDom();
+    _.buildBtn();
     _.initEvent();
-  }
-};
 
-Slider.prototype.buildBtn = function() {
-  let _ = this;
-
-  _.buttons = {
-    prevBtn: document.createElement('div'),
-    nextBtn: document.createElement('div')
+    window.onresize = function () {
+      _.buildDom();
+      _.initEvent();
+    }
   };
 
-  _.buttons.prevBtn.className = _.options.buttons.prev.classname;
-  _.buttons.nextBtn.className = _.options.buttons.next.classname;
+  Slider.prototype.buildBtn = function() {
+    let _ = this;
 
-  _.slider.appendChild(_.buttons.prevBtn);
-  _.slider.appendChild(_.buttons.nextBtn);
-};
+    _.buttons = {
+      prevBtn: document.createElement('div'),
+      nextBtn: document.createElement('div')
+    };
 
-Slider.prototype.buildDom = function() {
-  let _ = this,
-    sliderOuter = _.slider.getElementsByClassName('slider-outer')[0],
-    oldSliderWrap;
+    _.buttons.prevBtn.className = _.options.buttons.prev.classname;
+    _.buttons.nextBtn.className = _.options.buttons.next.classname;
 
-  if (sliderOuter !== undefined) {
-    oldSliderWrap = _.slider.getElementsByClassName('slider-wrapper')[0];
-  } else {
-    sliderOuter = document.createElement('div');
-    sliderOuter.className = 'slider-outer';
-    _.slider.appendChild(sliderOuter);
-  }
-  sliderOuter.appendChild(_.getDomSliderElements());
-  if (oldSliderWrap) {
-    oldSliderWrap.parentNode.removeChild(oldSliderWrap)
-  }
-};
+    _.slider.appendChild(_.buttons.prevBtn);
+    _.slider.appendChild(_.buttons.nextBtn);
+  };
 
-Slider.prototype.getDomSliderElements = function() {
-  let _ = this,
-    slideArr = _.slider.getElementsByClassName('slider-item'),
-    sliderWidth = _.slider.offsetWidth,
-    slideWidth = sliderWidth / _.getNeededCount(),
-    sliderWrapper = document.createElement('div'),
-    sliderWrapperWidth = 0;
+  Slider.prototype.buildDom = function() {
+    let _ = this,
+      sliderOuter = _.slider.getElementsByClassName('slider-outer')[0],
+      oldSliderWrap;
 
-  _.elements = [];
-  [].slice.call(slideArr).forEach(function(element) {
-    let slideStyle = element.currentStyle || window.getComputedStyle(element),
-      margin = parseFloat(slideStyle.marginLeft) + parseFloat(slideStyle.marginRight),
-      padding = parseFloat(slideStyle.paddingLeft) + parseFloat(slideStyle.paddingRight),
-      border = parseFloat(slideStyle.borderLeftWidth) + parseFloat(slideStyle.borderRightWidth),
-      supportWidth = (!isNaN(margin) ? margin : 0) +
-        (!isNaN(padding) ? padding : 0) +
-        (!isNaN(border) ? border : 0),
-      clearSlideWidth = slideWidth - (supportWidth / _.getNeededCount());
-
-    element.style.width = clearSlideWidth + 'px';
-    sliderWrapper.appendChild(element);
-    sliderWrapperWidth += (clearSlideWidth);
-    _.elements.push(element);
-  });
-
-  sliderWrapper.className = 'slider-wrapper';
-  sliderWrapper.style.width = sliderWrapperWidth + 'px';
-
-  return sliderWrapper;
-};
-
-Slider.prototype.initEvent = function() {
-  let _ = this,
-    translatePositionX = 0,
-    sliderWrapper = _.slider.getElementsByClassName('slider-wrapper')[0],
-    iterator,
-    nextElementCount = _.getNeededCount(),
-    prevElementCount;
-
-  _.buttons.nextBtn.addEventListener('click', function () {
-    for (
-      iterator = nextElementCount;
-      iterator < nextElementCount + _.getNeededCount() && iterator < _.elements.length;
-      iterator++
-    ) {
-      translatePositionX -= +_.elements[iterator].style.width.replace('px', '');
-    }
-    prevElementCount = iterator - _.getNeededCount();
-    nextElementCount = iterator;
-    if (iterator === _.elements.length) {
-      _.buttons.nextBtn.style.display = 'none';
+    if (sliderOuter !== undefined) {
+      oldSliderWrap = _.slider.getElementsByClassName('slider-wrapper')[0];
     } else {
-      _.buttons.nextBtn.style.display = 'block';
+      sliderOuter = document.createElement('div');
+      sliderOuter.className = 'slider-outer';
+      _.slider.appendChild(sliderOuter);
     }
-    _.buttons.prevBtn.style.display = 'block';
-    sliderWrapper.style.transform = 'translateX(' + translatePositionX + 'px)';
-  });
+    sliderOuter.appendChild(_.getDomSliderElements());
+    if (oldSliderWrap) {
+      oldSliderWrap.parentNode.removeChild(oldSliderWrap)
+    }
+  };
 
-  _.buttons.prevBtn.addEventListener('click', function () {
-    iterator = prevElementCount;
-    prevElementCount = iterator - _.getNeededCount();
-    for (
-      ;
-      iterator > (prevElementCount) && iterator > 0;
-      --iterator
-    ) {
-      translatePositionX += +_.elements[iterator - 1].style.width.replace('px', '');
-    }
-    nextElementCount = iterator + _.getNeededCount();
-    if (prevElementCount < 0) {
-      _.buttons.prevBtn.style.display = 'none';
-    } else {
+  Slider.prototype.getDomSliderElements = function() {
+    let _ = this,
+      slideArr = _.slider.getElementsByClassName('slider-item'),
+      sliderWidth = _.slider.offsetWidth,
+      slideWidth = sliderWidth / _.getNeededCount(),
+      sliderWrapper = document.createElement('div'),
+      sliderWrapperWidth = 0;
+
+    _.elements = [];
+    [].slice.call(slideArr).forEach(function(element) {
+      let slideStyle = element.currentStyle || window.getComputedStyle(element),
+        margin = parseFloat(slideStyle.marginLeft) + parseFloat(slideStyle.marginRight),
+        padding = parseFloat(slideStyle.paddingLeft) + parseFloat(slideStyle.paddingRight),
+        border = parseFloat(slideStyle.borderLeftWidth) + parseFloat(slideStyle.borderRightWidth),
+        supportWidth = (!isNaN(margin) ? margin : 0) +
+          (!isNaN(padding) ? padding : 0) +
+          (!isNaN(border) ? border : 0),
+        clearSlideWidth = slideWidth - (supportWidth / _.getNeededCount());
+
+      element.style.width = clearSlideWidth + 'px';
+      sliderWrapper.appendChild(element);
+      sliderWrapperWidth += (clearSlideWidth);
+      _.elements.push(element);
+    });
+
+    sliderWrapper.className = 'slider-wrapper';
+    sliderWrapper.style.width = sliderWrapperWidth + 'px';
+
+    return sliderWrapper;
+  };
+
+  Slider.prototype.initEvent = function() {
+    let _ = this,
+      translatePositionX = 0,
+      sliderWrapper = _.slider.getElementsByClassName('slider-wrapper')[0],
+      iterator,
+      nextElementCount = _.getNeededCount(),
+      prevElementCount;
+
+    _.buttons.nextBtn.addEventListener('click', function () {
+      for (
+        iterator = nextElementCount;
+        iterator < nextElementCount + _.getNeededCount() && iterator < _.elements.length;
+        iterator++
+      ) {
+        translatePositionX -= +_.elements[iterator].style.width.replace('px', '');
+      }
+      prevElementCount = iterator - _.getNeededCount();
+      nextElementCount = iterator;
+      if (iterator === _.elements.length) {
+        _.buttons.nextBtn.style.display = 'none';
+      } else {
+        _.buttons.nextBtn.style.display = 'block';
+      }
       _.buttons.prevBtn.style.display = 'block';
-    }
+      sliderWrapper.style.transform = 'translateX(' + translatePositionX + 'px)';
+    });
+
+    _.buttons.prevBtn.addEventListener('click', function () {
+      iterator = prevElementCount;
+      prevElementCount = iterator - _.getNeededCount();
+      for (
+        ;
+        iterator > (prevElementCount) && iterator > 0;
+        --iterator
+      ) {
+        translatePositionX += +_.elements[iterator - 1].style.width.replace('px', '');
+      }
+      nextElementCount = iterator + _.getNeededCount();
+      if (prevElementCount < 0) {
+        _.buttons.prevBtn.style.display = 'none';
+      } else {
+        _.buttons.prevBtn.style.display = 'block';
+      }
+      _.buttons.nextBtn.style.display = 'block';
+      sliderWrapper.style.transform = 'translateX(' + translatePositionX + 'px)';
+    });
     _.buttons.nextBtn.style.display = 'block';
-    sliderWrapper.style.transform = 'translateX(' + translatePositionX + 'px)';
-  });
-  _.buttons.nextBtn.style.display = 'block';
-  _.buttons.prevBtn.style.display = 'none';
-};
+    _.buttons.prevBtn.style.display = 'none';
+  };
 
-Slider.prototype.getNeededCount = function() {
-  let _ = this;
+  Slider.prototype.getNeededCount = function() {
+    let _ = this;
 
-  if (screen.width > _.options.counts.desktop.width) {
-    return _.options.counts.desktop.count;
-  } else if (screen.width > _.options.counts.tablet.width) {
-    return _.options.counts.tablet.count;
-  }
+    if (screen.width > _.options.counts.desktop.width) {
+      return _.options.counts.desktop.count;
+    } else if (screen.width > _.options.counts.tablet.width) {
+      return _.options.counts.tablet.count;
+    }
 
-  return _.options.counts.mobile.count;
-};
+    return _.options.counts.mobile.count;
+  };
 
-(function () {
-  let caseSliderContainer = document.getElementById('jsSlider'),
-    caseSliderContainer2 = document.getElementById('jsSlider2');
-
-  new Slider(caseSliderContainer);
-  new Slider(caseSliderContainer2);
+  window.Slider = Slider;
 })();
